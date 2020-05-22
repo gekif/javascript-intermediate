778241808b158c0efca235de155ebcba2ca1a797
Gekif <dzulfikar.maulana@gmail.com>
2020-05-22 22:42:07 +0700

    4. Project: 3. Images object setup

---
+++ project/js/modules.js
@@ -0,0 +1,16 @@
+var imageModule = (function (){
+    return {
+        carImages: {
+            car1: 'http://codingfaculty.com',
+            car2: 'http://codingfaculty.com',
+            car3: 'http://codingfaculty.com',
+            car4: 'http://codingfaculty.com',
+            car5: 'http://codingfaculty.com',
+            car6: 'http://codingfaculty.com',
+            car7: 'http://codingfaculty.com',
+            car8: 'http://codingfaculty.com'
+        },
+
+
+    };
+})();
